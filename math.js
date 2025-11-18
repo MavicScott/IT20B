0ac6c193527521492c3c3a604098030e45ed77ef
MavicScott <vismanosmavi@gmail.com>
2025-11-18 08:53:54 +0800

    Added user credental and  pricing ticket funtion

--- math.js
+++ math.js
@@ -3,24 +3,43 @@ function sum(num1,num2){
     console.log(sum);
 }
 
-sum(2,3);
 
 function diff(num1,num2){
     diff = num1 - num2;
     console.log(diff);
 }
 
-diff(2,3);
 
 function prod(num1,num2){
     prod = num1 * num2;
     console.log(prod);
 }
 
-prod(2,3);
 
 function qou(num1,num2){
     qou = num1 / num2;
     console.log(qou);
 }
+/*
+sum(2,3);
+diff(2,3);
+prod(2,3);
 qou(2,3);
+*/
+function add(num1,num2){
+    add = num1 + num2;
+}
+function subtract(num1,num2){
+    subtract = num1 - num2;
+}
+function  multiply(num1,num2){
+    multiply = num1 * num2;
+}
+function divide(num1,num2){
+    divide = num1 / num2;
+    console.log(divide);
+}
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
