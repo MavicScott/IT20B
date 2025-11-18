79581f69efee16600641c428f8e65c2bef2596ee
MavicScott <vismanosmavi@gmail.com>
2025-11-18 08:42:22 +0800

    Inital commit

---
+++ cs.js
@@ -0,0 +1,62 @@
+function LegalAge(age){
+    
+    if(age >= 18){
+        console.log("Your are adult");
+    }else{
+        console.log("Your are not adult");
+    }
+}
+
+//LegalAge(69);
+
+function computeGrade(grade){
+    if (grade >= 94, 5);
+    console.log("1");
+
+}else if(grade >= 91,5){
+    console.log("1.25");
+}else if(grade >= 89,5){
+    console.log("1.5");
+}else if(grade >= 86,5){
+    console.log("1.75");
+}else if(grade >= 84,5){
+    console.log("2");
+
+}else{
+    console.log("Not within the transmutation");
+}
+
+//computeGrade(87);
+
+function login(username,password){
+ 
+    if(username === "Mavic"){
+        console.log("Hello User");
+
+        if(password === 2513){
+        }else{
+            console.log("Invalid Login Credentials");
+        }
+    }else{
+        console.log("Invalid User");
+    }
+}
+
+//login("Mavic", 2513)
+
+function ticketPrice(age){
+    if(age <= 18){
+        if(age < 5){
+            console.log("Ticket: Free");
+        }else{
+            console.log("Ticket: Child Price");
+        }
+    }else{
+        if(age >= 69){
+            console.log("Ticket: Senior Price");
+        }else{
+            console.log("Ticket: Adult Price");
+        }
+    }
+}
+ticketPrice(70);
