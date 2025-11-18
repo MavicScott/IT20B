a14e9f630539e88681b4126c3a1c16dc242bab86
MavicScott <vismanosmavi@gmail.com>
2025-11-18 08:27:09 +0800

    Inital commit

---
+++ math.js
@@ -0,0 +1,26 @@
+function sum(num1,num2){
+    sum = num1 + num2;
+    console.log(sum);
+}
+
+sum(2,3);
+
+function diff(num1,num2){
+    diff = num1 - num2;
+    console.log(diff);
+}
+
+diff(2,3);
+
+function prod(num1,num2){
+    prod = num1 * num2;
+    console.log(prod);
+}
+
+prod(2,3);
+
+function qou(num1,num2){
+    qou = num1 / num2;
+    console.log(qou);
+}
+qou(2,3);
